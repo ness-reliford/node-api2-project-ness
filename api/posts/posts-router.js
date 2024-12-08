0df0d8b337eb7511854f315b7096188afed8b709
Ness Reliford <nessreliford@gmail.com>
2024-12-08 08:47:13 -0800

    posts-router skeleton

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -1,1 +1,28 @@
 // implement your posts router here
+const express = require('express')
+const router = express.Router()
+
+
+
+router.get('/', (req, res) => {
+
+})
+router.get('/:id', (req, res) => {
+
+})
+router.post('/', (req, res) => {
+
+})
+router.delete('/:id', (req, res) => {
+
+})
+router.put('/:id', (req, res) => {
+
+})
+router.get('/:id/messages', (req, res) => {
+
+})
+
+
+
+module.exports = router
