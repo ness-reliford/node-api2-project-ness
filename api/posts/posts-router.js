3818d9d8df867e454cdd35db7b668c3477ff9fb6
Ness Reliford <nessreliford@gmail.com>
2024-12-08 10:49:41 -0800

    POST new post

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -38,9 +38,32 @@ router.get('/:id', async (req, res) => {
         })
     }
 })
-router.post('/', (req, res) => {
 
+router.post('/', (req, res) => {
+    const { title, contents } = req.body
+    if(!title || !contents){
+        res.status(400).json({
+            message: "Please provide title and contents for the post"
+        })
+    } else {
+        Post.insert({ title, contents })
+            .then(({ id }) => {
+                return Post.findById(id)
+            })
+            .then(post => {
+                res.status(201).json(post)
+            })
+            .catch(err => {
+                res.status(500).json({
+                    message: "The posts information could not be retrieved",
+                    err: err.message,
+                    stack: err.stack,
+                })
+            })
+    }
+    
 })
+
 router.delete('/:id', (req, res) => {
 
 })
