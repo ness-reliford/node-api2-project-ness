05783adfc246bd8f3ea8e91ba40c115f3b11d340
Ness Reliford <nessreliford@gmail.com>
2024-12-08 08:54:12 -0800

    GET posts

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -1,11 +1,23 @@
 // implement your posts router here
 const express = require('express')
+
+const Post = require('./posts-model')
 const router = express.Router()
 
 
 
 router.get('/', (req, res) => {
-
+    Post.find()
+    .then(found => {
+        res.json(found)
+    })
+    .catch(err => {
+        res.status(500).json({
+            message: "The posts information could not be retrieved",
+            err: err.message,
+            stack: err.stack,
+        })
+    })
 })
 router.get('/:id', (req, res) => {
 
